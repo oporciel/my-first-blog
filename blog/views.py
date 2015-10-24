a16d0f10b909f8e28011ec90de9d3a093093c694
Omar.Porciel <oporciel@gmail.com>
2015-10-24 17:25:38 -0300

    modificacion de ejemplo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
+# crea las vistas para las bases
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
