cc34f3e41bd2e7db2bd1b16e67430a8787669f7e
Omar.Porciel <oporciel@gmail.com>
2015-11-05 05:42:12 -0300

    Últimas modificaciones...

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+#import pdb
+
 # crea las vistas para las bases
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+''' 
+Bueno ... aca estamos accediendo al nuevo post en el caso en que el request sea 
+un method "POST" se esta grabando y si no esta mostrando el ingreso de datos
+'''
+
+def post_new(request):
+    if request.method == "POST":
+	    form = PostForm(request.POST)
+	    if form.is_valid():
+	    	# supuestamente es un especie de punto de parada ja
+	    	#pdb.set_trace()
+	    	#print(request.user)
+	        post = form.save(commit=False)
+	        post.author = request.user
+	        post.save()
+	        return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
